38e0ff6d062ce91801feaf577a9002acd5e34d9b
lasource69 <55166718+lasource69@users.noreply.github.com>
2023-03-31 21:43:17 +1100

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,26 +1,3 @@
-const circleSpeed = 0.0001;
-
-let circles = [];
-const colours = [
-  [255, 200, 20, 2],
-  [10, 25, 100, 2],
-  [50, 100, 255, 2]
-];
-
-setup = () => {
-  createCanvas(windowWidth, windowHeight);
-  for (let i = 28; i--; ) {
-    const degree = random(TAU);
-    circles.push({
-      x: random(width),
-      y: random(height),
-      vx: cos(degree) * 4,
-      vy: sin(degree) * 4,
-      colour: colours[i % 3]
-    });
-  }
-};
-
 draw = () => {
   background(70, 15);
   noStroke();
@@ -42,17 +19,12 @@ draw = () => {
 
   // Set the font and style for "ART BASE"
   textStyle(BOLD);
-  fill(255, 255, 0);
+  fill(255, 255, 0); // Yellow color for "ART BASE"
   text("ART", width * 0.5 - fontSize * 1.6, height * 0.5 - fontSize * 0.25);
   text("BASE", width * 0.5 - fontSize * 1.6, height * 0.5 + fontSize * 0.75);
 
   // Set the font and style for ".digital"
   textStyle(NORMAL);
-  fill(255, 255, 255);
+  fill(255, 255, 255); // White color for ".digital"
   text(".digital", width * 0.5 - fontSize * 0.8, height * 0.5 + fontSize * 1.5);
-};
-
-// Handle window resizing
-windowResized = () => {
-  resizeCanvas(windowWidth, windowHeight);
 };
