74df7bef51d86cf817db561f6926fb3d6116c8df
lasource69 <55166718+lasource69@users.noreply.github.com>
2023-03-31 21:53:21 +1100

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,3 +1,26 @@
+const circleSpeed = 0.0001;
+
+let circles = [];
+const colours = [
+  [255, 200, 20, 2],
+  [10, 25, 100, 2],
+  [50, 100, 255, 2]
+];
+
+setup = () => {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 28; i--; ) {
+    const degree = random(TAU);
+    circles.push({
+      x: random(width),
+      y: random(height),
+      vx: cos(degree) * 4,
+      vy: sin(degree) * 4,
+      colour: colours[i % 3]
+    });
+  }
+};
+
 draw = () => {
   background(70, 15);
   noStroke();
@@ -28,3 +51,9 @@ draw = () => {
   fill(255, 255, 255); // White color for ".digital"
   text(".digital", width * 0.5 - fontSize * 0.8, height * 0.5 + fontSize * 1.5);
 };
+
+// Handle window resizing
+windowResized = () => {
+  resizeCanvas(windowWidth, windowHeight);
+};
+
