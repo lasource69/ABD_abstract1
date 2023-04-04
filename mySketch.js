f489cdc90f545f164e4591c6d59cc8db1ace8f39
lasource69 <55166718+lasource69@users.noreply.github.com>
2023-04-04 13:09:12 +1000

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,16 +1,11 @@
-const circleSpeed = 0.0001;
+const circleSpeed = .0001;
 
 let circles = [];
-const colours = [  [255, 200, 20, 2],
+const colours = [
+  [255, 200, 20, 2],
   [10, 25, 100, 2],
   [50, 100, 255, 2]
 ];
-
-let font;
-
-preload = () => {
-  font = loadFont('Roboto-Regular.ttf');
-};
 
 setup = () => {
   createCanvas(windowWidth, windowHeight);
@@ -19,8 +14,8 @@ setup = () => {
     circles.push({
       x: random(width),
       y: random(height),
-      vx: cos(degree) * 8,
-      vy: sin(degree) * 8,
+      vx: cos(degree) * 4,
+      vy: sin(degree) * 4,
       colour: colours[i % 3]
     });
   }
@@ -37,23 +32,23 @@ draw = () => {
     if (c.y < 0 || c.y > height) c.vy = -c.vy;
     // show
     fill(c.colour);
-    for (let i = 800; i > 0; i -= 4) ellipse(c.x, c.y, i, i);
+    for (let i = 800 * min(width, height) / 1080; i > 0; i -= 4) ellipse(c.x, c.y, i, i);
   }
 
   // Draw the text after drawing the circles
-  textSize(120);
-  textFont(font);
+  textSize(120 * min(width, height) / 1080);
+  textFont("Roboto");
 
   // Set the font and style for "ART BASE"
   textStyle(BOLD);
   fill(255, 255, 0);
-  text("ART", width * 0.5 - 130, height * 0.5);
-  text("BASE", width * 0.5 - 130, height * 0.5 + 80);
+  text("ART", width * 0.5 - 130 * width / 1920, height * 0.5);
+  text("BASE", width * 0.5 - 130 * width / 1920, height * 0.5 + 80 * height / 1080);
 
   // Set the font and style for ".digital"
   textStyle(NORMAL);
   fill(255, 255, 255);
-  text(".digital", width * 0.5 - 80, height * 0.5 + 160);
+  text(".digital", width * 0.5 - 80 * width / 1920, height * 0.5 + 160 * height / 1080);
 };
 
 // Handle window resizing
